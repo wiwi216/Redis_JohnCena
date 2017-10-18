ac5081416675c00fc0407cdcb9819e58165e450b
Max <max.hsieh@ehsn.com.tw>
2017-10-18 12:34:59 +0800

    頁面圖表js

--- Redis_JohnCena/App_Start/BundleConfig.cs
+++ Redis_JohnCena/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace Redis_JohnCena
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/echarts").Include(
+                      "~/Scripts/echarts.js"));
         }
     }
 }
